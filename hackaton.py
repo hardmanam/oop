d80d220ad91afbab8c8dd77260e98bd3ba3e9f30
hardmanam <hardmanam@mail.ru>
2022-11-04 19:49:27 +0600

    deleted else

--- hackaton.py
+++ hackaton.py
@@ -2,16 +2,16 @@ import json
 
 
 class RegisterMixin:
+    def __max_id(self,data):
+        if data:
+            ids = [i['id'] for i in data]
+            return max(ids) + 1
+        return 1
     def register(self, name, password):
-        @staticmethod
-        def __max_id(data):
-            if data:
-                ids = [i['id'] for i in data]
-                return max(ids) + 1
         if name in [user['name'] for user in self.data]:
             raise Exception('Такой юзер уже существует!')
         self.data.append({
-            'id': __max_id(self.data),
+            'id': self.__max_id(self.data),
             'name': name,
             'password': validate_password(password)
             })
@@ -19,59 +19,48 @@ class RegisterMixin:
         return 'Регистрация прошла успешно'
 
 class LoginMixin:
-
     def login(self, name, password):
         if name in [user['name'] for user in self.data]:
             user = [i for i in self.data if name == i['name']] 
             if password == user[0]['password']:
-                print('Вы успешно залогинились!')
-            else:
-                raise Exception('Неверный пароль!')  
-        else:
-            print('Нет такого юзера в БД') 
+                return 'Вы успешно залогинились!'
+            raise Exception('Неверный пароль!')
+        return 'Нет такого юзера в БД' 
 
 
 class ChangePasswordMixin:
     def change_password(self, name, password, new_password):        
         validate_password(new_password)
         user = [i for i in self.data if name == i['name']]
-
         if password == user[0]['password']:
             index = self.data.index(user[0])
             self.data[index]['password'] = new_password
             json.dump(self.data, open('user.json', 'w'), sort_keys=True, indent=4)
-            print('Успешно поменяли пароль')
-        else:
-            raise Exception('Старый пароль не совпадает')
+            return 'Успешно поменяли пароль'
+        raise Exception('Старый пароль не совпадает')
 
 class ChangeUserNameMixin:
+    def __correct_name(self,name):
+        names = [user['name'] for user in self.data]
+        while name in names:
+            print('Пользователь с наким именем существует')
+            name = input('Введите новое имя: ')
+        return name
     def change_name(self, name, new_name):
         names = [user['name'] for user in self.data]
-
         if name in names:
             user = [user for user in self.data if name == user['name']]
             index = self.data.index(user[0])
-
-            def __correct_name(name):
-                while name in names:
-                    print('Пользователь с наким именем существует')
-                    name = input('Введите новое имя: ')
-                return name
-            self.data[index]['name'] = __correct_name(new_name)
+            self.data[index]['name'] = self.__correct_name(new_name)
             json.dump(self.data, open('user.json', 'w'), sort_keys=True, indent=4)
-            print('Успешно сменили имя')
-        else:
-            raise Exception('Нет такого юзера в БД')
+            return 'Успешно сменили имя'
+        raise Exception('Нет такого юзера в БД')
 
 class CheckOwnerMixin:
-
     def check(self, owner):
         if owner in [user['name'] for user in self.data]:
-            print('Пост успешно создан')
-        else:
-            raise Exception('Нет такого пользователя')
-
-    
+            return 'Пост успешно создан'
+        raise Exception('Нет такого пользователя')
 
 
 class User(RegisterMixin, LoginMixin, ChangePasswordMixin, ChangeUserNameMixin):
@@ -97,11 +86,11 @@ def validate_password(pass1):
     return pass1
 
 obj1 = User()
-obj1.register('John111', '12345667ad')
-obj1.login('John111', '12345667ad')
-obj1.change_password('John111', '12345667ad', '123456789a')
-obj1.login('John111', '123456789a')
-obj1.change_name('John111', 'john321')
-John111 = Post('pen', 'green', 10, 1, 'john321')
-John111.post()
+print(obj1.register('John111', '12345667ad'))
+print(obj1.login('John111', '12345667ad'))
+print(obj1.change_password('John111', '12345667ad', '123456789a'))
+print(obj1.login('John111', '123456789a'))
+print(obj1.change_name('John111' , 'john321'))
+# John111 = Post('pen', 'green', 10, 1, 'john321')
+# John111.post()
 
